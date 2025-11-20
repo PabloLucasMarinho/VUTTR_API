ca2ec04644bb1eb9af43af4434e8d28b1afc0a8a
Pablo Marinho <pablolucas-1@outlook.com>
2025-11-20 08:27:53 -0300

    O usuário já pode criar um cadastro, fazer login e logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,36 +4,36 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-  /**
-   * Handle an incoming authentication request.
-   */
-  public function store(LoginRequest $request): Response
-  {
-    $request->authenticate();
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): array
+    {
+        $request->authenticate();
 
-    $user = $request->user();
-    $user->createToken('main')->plainTextToken;
+        $user = $request->user();
+        $token = $user->createToken('main')->plainTextToken;
 
-    return response()->noContent();
-  }
+        return [
+            'user' => new UserResource($user),
+            'token' => $token
+        ];
+    }
 
-  /**
-   * Destroy an authenticated session.
-   */
-  public function destroy(Request $request): Response
-  {
-    Auth::guard('web')->logout();
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): Response
+    {
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
 
-    $request->session()->invalidate();
-
-    $request->session()->regenerateToken();
-
-    return response()->noContent();
-  }
+        return response()->noContent();
+    }
 }
